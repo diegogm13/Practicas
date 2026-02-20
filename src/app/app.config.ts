a80616f33483b869c56db0fc08e37107c3c4c02d
diegogm13 <dg660262@gmail.com>
2026-02-20 13:05:32 -0600

    btn-primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeng/themes/lara';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideAnimations(),
+    providePrimeNG({
+      theme: {
+        preset: Lara
+      }
+    })
   ]
 };
